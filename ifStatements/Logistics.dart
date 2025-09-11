4f9c9d81b5bfc8350617b66666721c8ed90b56cb
OlamideHawanat123 <raheemhawanat@gmail.com>
2025-09-11 14:44:58 +0100

    finished logistics

---
+++ ifStatements/Logistics.dart
@@ -0,0 +1,35 @@
+import 'dart:io';
+
+main(){
+  List<int> commission = [120, 240, 360, 480];
+
+  stdout.writeln("Enter your commission rate for today");
+  String? input = stdin.readLineSync();
+
+  if(input == null || input.isEmpty){
+    print("no commision rate inputed");
+    return;
+  }
+
+  int commissionRate = int.parse(input);
+  int salary ;
+  int basePay = 5000;
+
+  if(commissionRate <= 50){
+    salary = commissionRate * commission[0] + basePay;
+  }
+
+  else  if(commissionRate <= 60){
+    salary = commissionRate * commission[1] + basePay;
+  }
+
+  else  if(commissionRate <= 70){
+    salary = commissionRate * commission[2] + basePay;
+  }
+
+  else{
+    salary = commissionRate * commission[3] + basePay;
+  }
+
+  stdout.writeln("Your salary is:  $salary");
+}
